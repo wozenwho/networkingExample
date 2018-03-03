1fba42ca2846694dbec065e22d0464cca16822df
root <root@localhost.localdomain>
2018-03-03 12:21:25 -0800

    Modified the example to use the HL wrapper functions.

--- connectionExample/Properties/HLClient.cs
+++ connectionExample/Properties/HLClient.cs
@@ -7,29 +7,37 @@ namespace Networking
 	{
 		/* Notes:
 		 * Call order: Client/Server.cs -> ServerLibrary.cs -> library.cpp -> server.cpp
+		 * 
+		 * 1) The library (libNetwork.so) is placed in the project folder:
+		 *      connectionExample/bin/Debug
+		 *     - for Unity, the library should be placed in /Assets/Plugins
          */
 
 
 		private static Int32 SOCKET_NODATA          = 0;
 		private static Int32 SOCKET_DATA_WAITING    = 1;
-        private static string destIP                = "192.168.0.42";
+        private static string destIP                = "142.232.18.12";
         private static ushort PORT_NO               = 9999;
         private static int MAX_BUFFER_SIZE          = 1200;
 
         private static Client client;
-		private static IntPtr serverInstance;
 		private static bool running;
 
         private static Thread recvThread;
         
-
+        // This class has Main by default. If you want to run the server example, un-main this
+        //  and main the ServerWrapper NotMain function.
 		public static void Main (string[] args)
 		{
-            client = new Client();
             byte[] sendBuffer = new byte[MAX_BUFFER_SIZE];
+
+            // Creates a new client object. 
+            // To use it, you must call the Client's Init function with a destination IP (server) and a port number.
+            client = new Client();
 
             // Initializes an EndPoint in the client object, returns 0 on success, -1 on fail. 
             Int32 result = client.Init(destIP, PORT_NO);
+
             if (result != 0)
             {
                 Console.WriteLine("Failed to initialize socket.");
@@ -39,22 +47,30 @@ namespace Networking
             running = true;
             recvThread.Start();
 
-            Int32 numRead;
-            for (int i = 0; i < 50; i++)
+            Console.WriteLine("Sending to EndPoint: " + destIP);
+            Int32 numSent;
+
+            // Sends 0 - z to the EndPoint.
+            for (int i = 48; i < 123; i++)
             {
                 for (int j = 0; j < 50; j++)
                 {
                     sendBuffer[j] = (byte) i;
                 }
                 // Send returns the number of bytes written
-                numRead = client.Send(sendBuffer, i + 1);
-                if (numRead <= 0)
+                numSent = client.Send(sendBuffer, i + 1);
+                if (numSent <= 0)
                 {
                     Console.WriteLine("Failed to write to socket.");
                 }
             }
 
+            System.Threading.Thread.Sleep(2000);
+            running = false;
+
 		} //End of Main
+
+
 
 		public static void recvThrdFunc()
 		{
@@ -67,14 +83,18 @@ namespace Networking
                 // If poll returns 0 (SOCKET_NODATA), there is no data waiting to be read
                 if (client.Poll() == SOCKET_DATA_WAITING)
                 {
+                    // Client.Recv returns number of read bytes.
                     numRead = client.Recv(recvBuffer, MAX_BUFFER_SIZE);
                     if (numRead <= 0)
                     {
+                        // Error if there is data waiting and we fail to read.
                         Console.WriteLine("Failed to read from socket.");
                     }
                     else
                     {
-                        Console.WriteLine("Read: " + recvBuffer);
+                        // Converts contents of recvBuffer to a string and print to console
+                        string contents = System.Text.Encoding.UTF8.GetString(recvBuffer);
+                        Console.WriteLine("Read: " + contents);
                     }
                 }
 
--- connectionExample/Properties/HLServer.cs
+++ connectionExample/Properties/HLServer.cs
@@ -9,8 +9,6 @@ namespace Networking
 		 * Call order: Client/Server.cs -> ServerLibrary.cs -> library.cpp -> server.cpp
          * A simple echo server implemented using our networking library wrapper functions.
          * 
-         * 
-         * POTENTIAL ISSUE WITH RECEIVE, ENDPOINT IS READ ONLY, CANNOT ASSIGN NEW VALUES.
          */
 		private static Int32 SOCKET_NODATA          = 0;
 		private static Int32 SOCKET_DATA_WAITING    = 1;
@@ -18,19 +16,23 @@ namespace Networking
         private static int MAX_BUFFER_SIZE          = 1200;
 
         private static Server server;
-		private static IntPtr serverInstance;
 		private static bool running;
 
         private static Thread recvThread;
         
-
-		public static void Main (string[] args)
+        // Make this the main function if you want to run the program as a server. 
+        // Also un-main the ClientWrapper class' Main function.
+		public static void NotMain(string[] args)
 		{
-            server = new Server();
             byte[] sendBuffer = new byte[MAX_BUFFER_SIZE];
 
-            // Initializes an EndPoint in the client object, returns 0 on success, -1 on fail. 
+            // Create a new Server object
+            // You must call the Server's Init method with a port number in order for the server to listen for packets.
+            server = new Server();
+
+            // Initializes an EndPoint in the server object, returns 0 on success, -1 on fail. 
             Int32 result = server.Init(PORT_NO);
+
             if (result != 0)
             {
                 Console.WriteLine("Failed to initialize socket.");
@@ -40,8 +42,16 @@ namespace Networking
             running = true;
             recvThread.Start();
 
+            System.Threading.Thread.Sleep(2000);
 		} //End of Main
 
+
+
+        // A simple thread function to read data from the EndPoint
+        /*
+         * NOTE: There might be an issue with Recv and passing a readonly EndPoint, but it has not been observed
+         * in testing. 
+         */
 		public static void recvThrdFunc()
 		{
             server = new Server();
@@ -54,21 +64,21 @@ namespace Networking
             {
                 // If poll returns 1 (SOCKET_DATA_WAITING), there is data waiting to be read
                 // If poll returns 0 (SOCKET_NODATA), there is no data waiting to be read
-                if (client.Poll() == SOCKET_DATA_WAITING)
+                if (server.Poll() == SOCKET_DATA_WAITING)
                 {
-                    numRead = client.Recv(&ep, recvBuffer, MAX_BUFFER_SIZE);
+                    numRead = server.Recv(&ep, recvBuffer, MAX_BUFFER_SIZE);
                     if (numRead <= 0)
                     {
                         Console.WriteLine("Failed to read from socket.");
                     }
                     else
                     {
-                        Console.WriteLine("Read: " + recvBuffer);
-                        // Console.WriteLine(ep.CAddr);
+                        string contents = System.Text.Encoding.UTF8.GetString(recvBuffer);
+                        Console.WriteLine("Read: " + contents);
+                        Console.WriteLine("From Endpoint: " + ep.addr.Byte3 + '.' + ep.addr.Byte2 + '.' + ep.addr.Byte1 + '.' + ep.addr.Byte0 + '\n');
                         server.Send(ep, recvBuffer, MAX_BUFFER_SIZE);
                     }
                 }
-
             }
 		} //End of recvThrdFunc
 	} // End of MainClass class
